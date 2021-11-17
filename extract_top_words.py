f180720f80d057a031e2ef5373e33d558fb0332e
Jacob Felknor <jacobfelknor073@gmail.com>
2021-11-17 13:27:23 -0700

    forgot to actually use N

--- extract_top_words.py
+++ extract_top_words.py
@@ -18,13 +18,13 @@ ex. Print top 25 words from java snippets
 
     python extract_top_words.py 25 snippets-java.csv
 """
-N = sys.argv[1]
+N = int(sys.argv[1])
 filename = sys.argv[2]
 print(f"Processing {filename}...")
 df = pd.read_csv(filename)
 # fill in missing values with an empty string
 df = df.fillna("")
 # word_counts = pd.Series(" ".join(df["snippet"]).lower().split()).value_counts()[:100]
-word_counts = Counter(" ".join(df["snippet"]).lower().split()).most_common(50)
-print("Top ten 'words'")
+word_counts = Counter(" ".join(df["snippet"]).lower().split()).most_common(N)
+print(f"Top {N} 'words'")
 print(word_counts)
